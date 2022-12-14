62de0488866ee5970b4e7f51c6cfac6e1610f5e8
abasaran <abasaran@innova.com.tr>
2022-12-14 10:34:26 +0300

    Last Update

--- app/src/main/java/com/basaran/howmanypeopleinspace/ui/HomeFragment.kt
+++ app/src/main/java/com/basaran/howmanypeopleinspace/ui/HomeFragment.kt
@@ -1,19 +1,16 @@
 package com.basaran.howmanypeopleinspace.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
 import com.basaran.howmanypeopleinspace.R
 import com.basaran.howmanypeopleinspace.databinding.FragmentHomeBinding
-import com.chuckerteam.chucker.api.ChuckerInterceptor
 import dagger.hilt.android.AndroidEntryPoint
-import okhttp3.OkHttpClient
-import kotlin.properties.Delegates
 
 
 @AndroidEntryPoint
